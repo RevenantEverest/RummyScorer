7b86727f9e2e6a46912a7bd6490246279fa35abc
Stefan Fischer <stefischer@mail.com>
2024-05-02 03:15:09 -0400

    Add custom markdown plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,16 @@ export default defineConfig({
     },
     plugins: [
         react(), 
-        TanStackRouterVite(), 
+        TanStackRouterVite(),
+        {
+            name: "markdown-loader",
+            transform(code, id) {
+                if (id.slice(-3) === ".md") {
+                    // For .md files, get the raw content
+                    return `export default ${JSON.stringify(code)};`;
+                }
+            }
+        },
         VitePWA({ 
             registerType: "autoUpdate",
             injectRegister: "auto",
