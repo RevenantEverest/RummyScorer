0cc13e41f2a5d53e282c970f3a31e42aa14522de
Stefan Fischer <stefischer@mail.com>
2024-05-02 03:18:23 -0400

    Create Markdown common component

--- src/components/Common/index.tsx
+++ src/components/Common/index.tsx
@@ -2,6 +2,7 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Layout } from './Layout';
+export { default as Markdown } from './Markdown';
 export { default as Modal } from './Modal';
 export { default as Tooltip } from './Tooltip';
 
