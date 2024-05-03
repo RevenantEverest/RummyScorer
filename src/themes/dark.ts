34c38d8051335ff25d3563d7eaee285382d208ce
Stefan Fischer <stefischer@mail.com>
2024-05-03 01:59:25 -0400

    Update dark theme colors

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -1,7 +1,6 @@
 import type { Theme, ThemeColors } from '@@types/theme.js';
 
 const colors: ThemeColors = {
-    // primary: "#d86bfe",
     primary: "#cb3bfc",
     secondary: "#ac010b",
     accent: "#fd872c",
@@ -9,7 +8,7 @@ const colors: ThemeColors = {
     card: "#381545",
     cardLight: "#200015",
     text: "#f4deff",
-    muted: "#92849a"
+    muted: "#7a6e80"
 };
 
 const dark: Theme = {
